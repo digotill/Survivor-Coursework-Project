25a60088d82c4dff245e14e27b99bbe9614ca9f2
digotill <digotill@gmail.com>
2024-12-12 18:38:51 +0000

    maybe the player movement bug is finally fixed

--- Code/Display/Window.py
+++ Code/Display/Window.py
@@ -15,12 +15,20 @@ class Window(main):
                     self.rect = pygame.Rect(self.pos.x, self.pos.y, self.res[0], self.res[1])
                     self.offset_rect = self.rect.copy()
 
-
           def move(self, dx, dy, move_horizontally, move_vertically):
+                    # Update window position
+                    if move_horizontally:
+                              self.pos.x += dx * self.game.player.current_vel * self.game.dt
+                    if move_vertically:
+                              self.pos.y += dy * self.game.player.current_vel * self.game.dt
 
-                    new_x = self.pos.x + dx * self.game.player.current_vel * self.game.dt
-                    new_y = self.pos.y + dy * self.game.player.current_vel * self.game.dt
+                    self.update_mouse_smoothing()
+                    rounded_offset = self.calculate_offset()
+                    shake_offset = self.calculate_shake()
+                    self.update_offset_rect(rounded_offset, shake_offset)
+                    self.ensure_player_in_bounds()
 
+          def update_mouse_smoothing(self):
                     mouse_target = v2(self.game.correct_mouse_pos[0] - 0.5 * REN_RES[0],
                                       self.game.correct_mouse_pos[1] - 0.5 * REN_RES[1])
 
@@ -28,55 +36,49 @@ class Window(main):
                               self.lerp(self.mouse_smoothing.x, mouse_target.x,
                                         self.window_mouse_smoothing_amount * self.game.dt),
                               self.lerp(self.mouse_smoothing.y, mouse_target.y,
-                                        self.window_mouse_smoothing_amount * self.game.dt))
+                                        self.window_mouse_smoothing_amount * self.game.dt)
+                    )
 
-                    if abs(self.mouse_smoothing.x) < self.deadzone:
-                              self.mouse_smoothing.x = 0
-                    if abs(self.mouse_smoothing.y) < self.deadzone:
-                              self.mouse_smoothing.y = 0
+                    self.mouse_smoothing.x = 0 if abs(
+                              self.mouse_smoothing.x) < self.deadzone else self.mouse_smoothing.x
+                    self.mouse_smoothing.y = 0 if abs(
+                              self.mouse_smoothing.y) < self.deadzone else self.mouse_smoothing.y
 
-                    self.target_offset.x = self.window_max_offset * int(self.mouse_smoothing.x)
-                    self.target_offset.y = self.window_max_offset * int(self.mouse_smoothing.y)
+          def calculate_offset(self):
+                    self.target_offset = v2(
+                              self.window_max_offset * int(self.mouse_smoothing.x),
+                              self.window_max_offset * int(self.mouse_smoothing.y)
+                    )
 
                     self.current_offset = v2(
                               self.lerp(self.current_offset.x, self.target_offset.x, self.lerp_speed * self.game.dt),
-                              self.lerp(self.current_offset.y, self.target_offset.y, self.lerp_speed * self.game.dt))
+                              self.lerp(self.current_offset.y, self.target_offset.y, self.lerp_speed * self.game.dt)
+                    )
 
-                    rounded_offset = v2(round(self.current_offset.x), round(self.current_offset.y))
+                    return v2(round(self.current_offset.x), round(self.current_offset.y))
 
-                    if (move_horizontally and 0 < new_x < GAME_SIZE[0] - self.res[0] and
-                            self.res[0] / 2 < self.game.player.pos.x < GAME_SIZE[0] - self.res[0] / 2):
-                              self.pos.x = new_x
-                    if (move_vertically and 0 < new_y < GAME_SIZE[1] - self.res[1] and
-                            self.res[1] / 2 < self.game.player.pos.y < GAME_SIZE[1] - self.res[1] / 2):
-                              self.pos.y = new_y
-
-                    if 0 < self.pos.x + rounded_offset.x < GAME_SIZE[0] - self.res[0]:
-                              self.offset_rect.x = self.pos.x + rounded_offset.x
-                    if 0 < self.pos.y + rounded_offset.y < GAME_SIZE[1] - self.res[1]:
-                              self.offset_rect.y = self.pos.y + rounded_offset.y
-
-                    shake_offset = self.calculate_shake()
-
+          def update_offset_rect(self, rounded_offset, shake_offset):
                     self.offset_rect.x = max(0, min(self.pos.x + rounded_offset.x + shake_offset.x,
                                                     GAME_SIZE[0] - self.res[0]))
                     self.offset_rect.y = max(0, min(self.pos.y + rounded_offset.y + shake_offset.y,
                                                     GAME_SIZE[1] - self.res[1]))
 
-                    player_left = self.game.player.pos.x - self.offset_rect.x - self.game.player.res[0] / 2
-                    player_right = player_left + self.game.player.res[0]
-                    player_top = self.game.player.pos.y - self.offset_rect.y - self.game.player.res[1] / 2
-                    player_bottom = player_top + self.game.player.res[1]
+          def ensure_player_in_bounds(self):
+                    player = self.game.player
+                    player_left = player.pos.x - self.offset_rect.x - player.res[0] / 2
+                    player_right = player_left + player.res[0]
+                    player_top = player.pos.y - self.offset_rect.y - player.res[1] / 2
+                    player_bottom = player_top + player.res[1]
 
-                    if player_left < self.game.player.offset_x1:
-                              self.offset_rect.x += player_left - self.game.player.offset_x1
-                    elif player_right > self.res[0] - self.game.player.offset_x2:
-                              self.offset_rect.x += player_right - (self.res[0] - self.game.player.offset_x2)
+                    if player_left < player.offset_x1:
+                              self.offset_rect.x += player_left - player.offset_x1
+                    elif player_right > self.res[0] - player.offset_x2:
+                              self.offset_rect.x += player_right - (self.res[0] - player.offset_x2)
 
-                    if player_top < self.game.player.offset_y1:
-                              self.offset_rect.y += player_top - self.game.player.offset_y1
-                    elif player_bottom > self.res[1] - self.game.player.offset_y2:
-                              self.offset_rect.y += player_bottom - (self.res[1] - self.game.player.offset_y2)
+                    if player_top < player.offset_y1:
+                              self.offset_rect.y += player_top - player.offset_y1
+                    elif player_bottom > self.res[1] - player.offset_y2:
+                              self.offset_rect.y += player_bottom - (self.res[1] - player.offset_y2)
 
                     self.offset_rect.x = max(0, min(self.offset_rect.x, GAME_SIZE[0] - self.res[0]))
                     self.offset_rect.y = max(0, min(self.offset_rect.y, GAME_SIZE[1] - self.res[1]))
